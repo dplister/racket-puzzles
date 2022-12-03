4a7ff0276bee4cb6d1e82f5d5e0365644b5aad1a
dev <dev@surface>
2022-12-03 20:03:59 +1000

    Day Three Part B Complete

--- advent/2022/day_three.rkt
+++ advent/2022/day_three.rkt
@@ -2,7 +2,7 @@
 
 (define (compartments input)
   "splits input into two character lists"
-  (split-at (string->list input) (/ (string-length input) 2)))
+  (split-at input (/ (length input) 2)))
 
 (define (common-items rucksack)
   "finds the items that are common to both parts of rucksack"
@@ -31,27 +31,43 @@
   (apply + (map score-rucksack inputs)))
 
 (define (get-input)
-  (file->lines "day_three.txt"))
+  (map string->list (file->lines "day_three.txt")))
 
 (define (part-a)
   (total-rucksacks (get-input)))
 
 (part-a)
 
+; part b
+
+(define (common-group-item rucksacks)
+  (apply set-intersect rucksacks))
+
+(define (score-groups lines [acc 0])
+  (if (empty? lines) acc
+    (score-groups (drop lines 3) 
+		  (apply + acc (map score (common-group-item (take lines 3)))))))
+
+(define (part-b)
+  (score-groups (get-input)))
+
+(part-b)
+
 (module+ test
   (require rackunit)
 
-  (let-values ([(left right) (compartments "asDF")])
+  (let-values ([(left right) (compartments (string->list "asDF"))])
     (check-equal? left '(#\a #\s))
     (check-equal? right '(#\D #\F)))
 
   (define examples
-    '("vJrwpWtwJgWrhcsFMMfFFhFp"
-      "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
-      "PmmdzqPrVvPwwTWBwg"
-      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
-      "ttgJtRGJQctTZtZT"
-      "CrZsJsPPZsGzwwsLwLmpwMDw"))
+    (map string->list 
+	 '("vJrwpWtwJgWrhcsFMMfFFhFp"
+	   "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+	   "PmmdzqPrVvPwwTWBwg"
+	   "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+	   "ttgJtRGJQctTZtZT"
+	   "CrZsJsPPZsGzwwsLwLmpwMDw")))
 
   (for-each (lambda (input expected)
 	      (check-equal? (common-items input) expected))
@@ -69,5 +85,7 @@
 	    '(16 38 42 22 20 19))
 
   (check-equal? (total-rucksacks examples) 157)
+
+  (check-equal? (score-groups examples) 70)
 
 )
