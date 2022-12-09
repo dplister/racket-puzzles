bc1e32c97ccf5ec774e0b3d94769cc8d13f94ebc
Dev <dev@box>
2022-12-09 16:29:16 +1000

    Missing one location

--- advent/2022/day_eight.rkt
+++ advent/2022/day_eight.rkt
@@ -21,16 +21,18 @@
 (define (find-visible-treeline trees)
   "finds all trees that are visible in line"
   (filter-map (lambda (i) 
-		(and (or (is-visible-left (take trees i))
-			 (is-visible-right (drop trees (sub1 i))))
+		(and (or (is-visible-left (take trees (add1 i)))
+			 (is-visible-right (drop trees i)))
 		     (list-ref trees i)))
 	  (range 0 (length trees))))
 
 (define (is-visible-left ls)
+  (displayln (format "LEFT: ~v" ls))
   (or (empty? ls)
       (apply > (reverse (map location-z ls)))))
 
 (define (is-visible-right ls)
+  (displayln (format "RIGHT: ~v" ls))
   (or (empty? ls)
       (apply > (map location-z ls))))
 
@@ -46,8 +48,15 @@
 	    "35390")
       parse-number))
 
-  (check-equal? (find-visible-treeline (list (location 0 1 1) (location 0 1 2) (location 0 2 3)))
-		(list (location 0 1 1) (location 0 1 2) (location 0 2 3)))
+;  (check-equal? (find-visible-treeline (list (location 0 1 1) (location 0 1 2) (location 0 2 3)))
+;		(list (location 0 1 1) (location 0 1 2) (location 0 2 3)))
 
-  (check-equal? (length (find-visible-trees example)) 21)
+  ;(check-equal? (find-visible-treeline (list (location 0 0 3) (location 0 1 2) (location 0 1 2) (location 0 2 3)))
+;		(list (location 0 0 3) (location 0 2 3)))
+
+  ;(check-equal? (length (find-visible-trees example)) 21)
+
+  (for-each (lambda (t) 
+	      (displayln (format "X: ~v Y: ~v Z: ~v" (point-x t) (point-y t) (location-z t))))
+	    (find-visible-trees example))
 )
